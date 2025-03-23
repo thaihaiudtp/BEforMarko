b12093f282118b59454bcd859e49d455b745329c
thaihaiudtp <nguyenmanhthaiha@gmail.com>
2025-03-23 16:53:18 +0700

    fix chat1

--- src/db.js
+++ src/db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 async function connectDB() {
     try {
         await mongoose.connect(process.env.WEB_DATABASE_URL, {
-            serverSelectionTimeoutMS: 5000, // Giữ timeout để tránh treo kết nối
+            serverSelectionTimeoutMS: 30000, // Giữ timeout để tránh treo kết nối
+            connectTimeoutMS: 30000,
         });
         console.log("✅ MongoDB connected successfully!");
     } catch (error) {
