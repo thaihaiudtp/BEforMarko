a8e82eb67c7cc3b53c7d96609f38d76a24fdff24
thaihaiudtp <nguyenmanhthaiha@gmail.com>
2025-06-03 00:15:31 +0700

    fix image too lagre

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ require('dotenv').config();
 const connect = require('./db');
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(session({
   secret: process.env.SECRET_KEY,  // Chuỗi bí mật để mã hóa session
   resave: false,              // Không lưu session nếu không thay đổi
